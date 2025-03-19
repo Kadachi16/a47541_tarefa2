9f3a7fabbb663840831d4cc778161591431b0cde
Kadachi16 <a47541@alunos.ipb.pt>
2025-03-19 17:10:16 +0000

    Read input from a keyboard

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,6 +12,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontal = 0.0f;
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+
+            horizontal = -1.0f;
+
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+
+            horizontal = 1.0f;
+
+        }
+
+        Debug.Log(horizontal);
+
         Vector2 position = transform.position;
         position.x = position.x + 0.1f;
         transform.position = position;
