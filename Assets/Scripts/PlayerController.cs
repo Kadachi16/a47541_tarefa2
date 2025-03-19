7e9fe4489e6e8a7678577992630a52acb8872a6a
Kadachi16 <a47541@alunos.ipb.pt>
2025-03-19 17:27:35 +0000

    up arrow

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,7 +30,7 @@ public class PlayerController : MonoBehaviour
         Debug.Log(horizontal);
 
         float vertical = 0.0f;
-        if (Keyboard.current.downArrowKey.isPressed)
+        if (Keyboard.current.upArrowKey.isPressed)
         {
             vertical = -1.0f;
         }
