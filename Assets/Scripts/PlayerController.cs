e2f7040c1b059841971e942608a6cb4c56118755
Kadachi16 <a47541@alunos.ipb.pt>
2025-03-19 19:00:22 +0000

    Adjust the speed of movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,8 +11,7 @@ public class PlayerController : MonoBehaviour
 
     {
         MoveAction.Enable();
-        //QualitySettings.vSyncCount = 0;
-        //Application.targetFrameRate = 10;
+       
     }
 
     // Update is called once per frame
@@ -21,7 +20,7 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * 0.1f * Time.deltaTime;
+        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
         transform.position = position;
     }
 }
